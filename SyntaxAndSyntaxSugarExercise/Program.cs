1091db6efec5d566a4e4ec3c99a70561e733d2fe
maxwell1565 <146258976+maxwell1565@users.noreply.github.com>
2024-02-20 11:49:37 -0600

    Syntax exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+﻿using System.ComponentModel;
+
+namespace SyntaxAndSyntaxSugarExercise
 {
     public class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello, World!");
+           var answer = 4;
+     
+           string response = answer < 9 ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
+           Console.WriteLine(response);
         }
     }
 }
